6fd390d7e94e587af1d808d5e8f2bb48e4332282
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-08-24 17:40:29 -0300

    feat: added base pagination component, added a arrow image to assets folder

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,8 +1,14 @@
 const Pagination = () => {
     return (
-        <>
-            
-        </>
+        <ul>
+            <li></li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li></li>
+        </ul>
     )
 }
 
