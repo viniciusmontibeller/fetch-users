e804d21c04146e394250e6e03dacdc7374f6d1e7
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-08-27 18:27:24 -0300

    style: added styles to user details page

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -7,40 +7,69 @@ const UserDetails = () => {
     console.log(state)
 
     return (
-        <>
+        <main>
             <Link to=".." relative="path">
                 Back
             </Link>
-            <div>
+            <div className="user-primary-info">
                 <img src={state.picture.large} alt=""/>
-                <p>{`${state.name.first} ${state.name.last}`}</p>
-                <p>{state.name.title}</p>
+                <p className="name">{`${state.name.first} ${state.name.last}`}</p>
+                <p className="title">{state.name.title}</p>
             </div>
-            <div>
-                <p>Info</p>
-                <p>Location</p>
-                <p>Login</p>
-            </div>
-            <div className="info">
-                <p>{state.gender}</p>
-                <p>{state.email}</p>
-                <p>{state.phone}</p>
-                <p>{state.dob.age}</p>
-            </div>
-            <div className="location">
-                <p>{state.location.city}</p>
-                <p>{state.location.country}</p>
-                <p>{state.location.state}</p>
-                <p>{`${state.location.street.name} ${state.location.street.number}`}</p>
-                <p>{state.location.postcode}</p>
-            </div>
-            <div className="login">
-                <p>{state.login.username}</p>
-                <p>{state.login.password}</p>
-                <p>{state.registered.age}</p>
-                <p>{transformStringToDate(state.registered.date)}</p>
-            </div>
-        </>
+            <section className="user-info-tabs">
+                <div className="tabs">
+                    <button>Info</button>
+                    <button>Location</button>
+                    <button>Login</button>
+                </div>
+                <div className="tab info active">
+                    <div>
+                        <p>Gender</p>
+                        <p>{state.gender}</p>
+                    </div>
+                    <div>
+                        <p>Email</p>
+                        <p>{state.email}</p>
+                    </div>
+                    <div>
+                        <p>Phone</p>
+                        <p>{state.phone}</p>
+                    </div>
+                    <div>
+                        <p>Age</p>
+                        <p>{state.dob.age}</p>
+                    </div>
+                </div>
+                <div className="tab location">
+                    <div>
+                        <p>City</p>
+                        <p>{state.location.city}</p>
+                    </div>
+                    <div>
+                        <p>Country</p>
+                        <p>{state.location.country}</p>
+                    </div>
+                    <div>
+                        <p>State</p>
+                        <p>{state.location.state}</p>
+                    </div>
+                    <div>
+                        <p>Street</p>
+                        <p>{`${state.location.street.name} ${state.location.street.number}`}</p>
+                    </div>
+                    <div>
+                        <p>Postcode</p>
+                        <p>{state.location.postcode}</p>
+                    </div>
+                </div>
+                <div className="tab login">
+                    <p>{state.login.username}</p>
+                    <p>{state.login.password}</p>
+                    <p>{state.registered.age}</p>
+                    <p>{transformStringToDate(state.registered.date)}</p>
+                </div>
+            </section>
+        </main>
     )
 }
 
